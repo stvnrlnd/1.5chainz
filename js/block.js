d61bb162e4e0988420575d7969d5db0e11eeb7d1
Steven Roland <steven@mojave.io>
2018-03-08 16:12:39 -0500

    Add explanation of what the block class does and why it is needed. Also add blockchan.js file for blockchain class

--- js/block.js
+++ js/block.js
@@ -1,6 +1,15 @@
 const SHA256 = require('sha256');
 
 class Block {
+    /*
+     * Initialize the properties of the block. 
+     *
+     *   Each block is given an index that tells 
+     *   us at what position the block sits on the chain. 
+     *   We also include a timestamp, some data 
+     *   to store in our block, and finally the 
+     *   hash of the previous block.
+     */
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.previousHash = previousHash;
