d34235cff87d6d0efa1be9cc2c87a284d4729513
Steven Roland <steven@mojave.io>
2018-03-09 10:02:55 -0500

    Require Block class and use constructor to build first block

--- js/blockchain.js
+++ js/blockchain.js
@@ -1,5 +1,12 @@
+// Require Block class
+const Block = require('block');
+
 class Blockchain {
     constructor() {
-        
+        this.chain = [this.createGenesisBlock()]; // Make sure to create first block on start
+    }
+
+    createGenesisBlock() { // Create first block with Block class
+        return new Block(0, "03/09/2018", "Genesis Block", "0");
     }
 }
