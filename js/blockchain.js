998995b5ca4baf04312bd284ebdc0effe5a75a80
Steven Roland <steven@mojave.io>
2018-03-09 10:06:11 -0500

    Add getLatestBlock method

--- js/blockchain.js
+++ js/blockchain.js
@@ -9,4 +9,10 @@ class Blockchain {
     createGenesisBlock() { // Create first block with Block class
         return new Block(0, "03/09/2018", "Genesis Block", "0");
     }
+
+    getLatestBlock() { // Find out what the last block was to make hash for next block
+        return this.chain = [this.chain.length - 1];
+    }
+
+
 }
