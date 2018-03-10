96906a7deaa1ffa59f2a586b225fda251c20648c
Steven Roland <steven@mojave.io>
2018-03-09 21:52:28 -0500

    Add pandingTransactions array and set mining reward. Add method to create genesis block

--- php/blockchain.php
+++ php/blockchain.php
@@ -5,7 +5,11 @@ class Blockchain {
     public function __construct() {
         $this->chain = [$this->createGenesisBlock()];
         $this->difficulty = 5;
+        $this->pendingTransactions = [];
+        $this->miningReward = 10;
     }
 
-    
+    private function createGenesisBlock() {
+        return new Block(date('Y-m-d H:i:s'), [], "0");
+    }
 }
