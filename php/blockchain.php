96b9a26169af895cd35cf024a141f7fd270b4a20
Steven Roland <steven@mojave.io>
2018-03-09 21:39:43 -0500

    Require Block class. Add Blockchain class properties.

--- php/blockchain.php
+++ php/blockchain.php
@@ -1,1 +1,11 @@
 <?php
+require_once('./block.php');
+
+class Blockchain {
+    public function __construct() {
+        $this->chain = [$this->createGenesisBlock()];
+        $this->difficulty = 5;
+    }
+
+    
+}
