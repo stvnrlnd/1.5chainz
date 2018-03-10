51e85ecf25022f0df49cec3bfb661f7cdae69099
Steven Roland <steven@mojave.io>
2018-03-09 21:30:39 -0500

    Add properties for Block class

--- php/block.php
+++ php/block.php
@@ -1,1 +1,15 @@
 <?php
+class Block {
+    public $nonce;
+    public function __construct($timestamp, $transactions, $previousHash = null) {
+        $this->timestamp = $timestamp;
+        $this->transactions = $transactions;
+        $this->previousHash = $previousHash;
+        $this->hash = $this->calculateHash();
+        $this->nonce = 0;
+    }
+
+    public function calculateHash() {
+        return hash("sha256", $this->previousHash.$this->timestamp.((string)$this->transactions).$this->nonce;
+    }
+}
