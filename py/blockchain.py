643ca1f873c06b848f2bdaf9d2b5754f23e18f89
Steven Roland <steven@mojave.io>
2018-03-12 13:47:12 -0400

    Add some helper methods for creating a block and adding it to the chain

--- py/blockchain.py
+++ py/blockchain.py
@@ -2,3 +2,17 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+    def new_block(self):
+        pass
+    
+    def new_transactions(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+    
+    @property
+    def last_block(self):
+        pass
